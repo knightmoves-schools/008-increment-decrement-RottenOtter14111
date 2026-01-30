61680112015cbf49bcff563fdf787fb6c6d1cf17
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-01-30 18:03:19 +0000

    done?

--- Counter.cs
+++ Counter.cs
@@ -3,6 +3,8 @@ public class Counter
 {
     public static int Visitors = 100;
 
-    // Add your code here
-    
+    public int PrefixIncrement => ++Visitors;
+    public int PostfixIncrement => Visitors++;
+    public int PrefixDecrement => --Visitors;
+    public int PostfixDecrement => Visitors--;
 }
