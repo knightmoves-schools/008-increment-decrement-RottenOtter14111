fc549b41a084c29ec60bf0f4e2b025be8ffd5927
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-01-30 18:14:02 +0000

    WORK

--- Counter.cs
+++ Counter.cs
@@ -3,39 +3,8 @@ public class Counter
 {
     public static int Visitors = 100;
 
-    public int PrefixIncrement
-    {
-        get
-        {
-            Visitors = 100;
-            return ++Visitors;
-        }
-    }
-
-    public int PostfixIncrement
-    {
-        get
-        {
-            Visitors = 100;
-            return Visitors++;
-        }
-    }
-
-    public int PrefixDecrement
-    {
-        get
-        {
-            Visitors = 100;
-            return ++Visitors;
-        }
-    }
-
-    public int PostfixDecrement
-    {
-        get
-        {
-            Visitors = 100;
-            return Visitors++;
-        }
-    }
+    public int PrefixIncrement => ++Visitors;
+    public int PostfixIncrement => Visitors++;
+    public int PrefixDecrement => --Visitors;
+    public int PostfixDecrement => Visitors--;
 }
